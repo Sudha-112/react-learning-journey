03da2997841cc9ed4589fe7bae34b08b2aca4f1f
sudha <sudhapalyal81@gmail.com>
2026-08-01 15:55:58 +0530

    basic react and vite react project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+
+
+function App() {
+  return (
+   <h1>chai aur react | Sudha</h1>
+ 
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+
+function App() {
+ 
+  return (
+  <h1>Hello World with vite| Sudha</h1>
+  )
+}
+
+export default App
