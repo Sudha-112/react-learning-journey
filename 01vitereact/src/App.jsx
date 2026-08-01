14cd0265e1537104f17536f561caa58825706431
sudha <sudhapalyal81@gmail.com>
2026-08-01 22:56:17 +0530

    create react library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,11 @@ import Chai from "./Chai"
 
 
 function App() {
- 
+  const username = "Sudha";
   return (
     <>
   <Chai/>
-  <h1>chai aur react</h1>
+  <h1>chai aur react with {username}</h1>
   <p>Hi , its me sudha</p>
   </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,40 @@
-import REACT from 'react'
-import REACTDOM from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-REACTDOM.createRoot(document.getElementById('root')).render(
+function MyApp(){
+    return (
+      <div>
+        <h1>Custom App !</h1>
+      </div>
+    )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target ="_blank">Visit google</a>
+)
+
+const anotherUser = "Sudha";
+
+const reactElement = React.createElement(
+      'a',
+      {href: 'https://google.com',target: '_blank'},
+      'Click me to visit google',
+      anotherUser  // evaluated expression
+)
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
  
-    <App />
-  ,
+  reactElement
+  
 )
