9032a44055db2609e425d9f19a6538656913ab24
sudha <sudhapalyal81@gmail.com>
2026-08-03 15:24:40 +0530

    learn useCallback, useRef, useState and useEffect

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
    
      setPassword(pass);
 
-  } , [length, numberAllowed, charAllowed, setPassword]);
+  } , [length, numberAllowed, charAllowed]);
 
   const copyPasswordToClipboard = useCallback( () => {
     passwordRef.current?.select();
