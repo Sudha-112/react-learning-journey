c6543084d50328b70402397917a13c4a120f7d55
sudha <sudhapalyal81@gmail.com>
2026-08-06 12:22:12 +0530

    React Router

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+
+  const data = useLoaderData();
+  // const[data, setData] = useState([]);
+  // useEffect(() => {
+  //     fetch('https://api.github.com/users/sudha-112')
+  //     .then((res) => res.json())
+  //     .then(data => {
+  //       console.log(data);
+  //       setData(data);
+  //     }
+  //      )
+  // },[])
+
+   {
+    return (
+      <div className='text-center bg-gray-600 m-4  text-white p-4 text-3xl'>Github followers: {data.followers}
+      <img src={data.avatar_url} alt="Git_image" width={250} />
+      </div>
+    )
+  }
+}
+
+export default Github
+
+export const githubInfoLoader = async () =>{
+    const response = await fetch('https://api.github.com/users/sudha-112')
+    return response.json()
+}
